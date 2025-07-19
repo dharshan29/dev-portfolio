331cde1cfe8aae1134ba6ad7f30b9cc90d6b71ae
dharshan29 <dharshangowda2000@gmail.com>
2025-07-19 20:53:15 +0530

    border fix

--- components/chat/chat.tsx
+++ components/chat/chat.tsx
@@ -284,7 +284,7 @@ const Chat = () => {
       <div className="container mx-auto flex h-full max-w-3xl flex-col ">
         {/* Scrollable Chat Content */}
         <div
-          className="flex-1 overflow-y-auto px-2 z-50"
+          className="flex-1 overflow-y-auto px-2 z-50 mb-[84px]"
           // style={{ paddingTop: `${headerHeight}px` }}
         >
           <AnimatePresence mode="wait">
@@ -322,7 +322,7 @@ const Chat = () => {
         </div>
 
         {/* Fixed Bottom Bar */}
-        <div className="sticky bottom-0 z-50 bg-white px-2 pt-3 md:px-0 md:pb-4">
+        <div className="sticky bottom-0 z-50 px-2 pt-3 md:px-0 md:pb-4">
           <div className="relative flex flex-col items-center gap-3">
             <HelperBoost submitQuery={submitQuery} setInput={setInput} />
             <ChatBottombar
